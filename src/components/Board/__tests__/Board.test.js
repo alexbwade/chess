49842dc463ba6ba153090972bda57f0190be1dff
Alex Wade <alexbwade@gmail.com>
2022-04-28 08:03:29 -0700

    fixup: wrong num squares in test name

--- src/components/Board/__tests__/Board.test.js
+++ src/components/Board/__tests__/Board.test.js
@@ -2,11 +2,13 @@ import { render, screen } from '@testing-library/react';
 
 import Board from '../Board';
 
-test('renders 52 squares', () => {
+const NUM_SQUARES = 64;
+
+test(`renders ${NUM_SQUARES} squares`, () => {
   render(<Board />);
 
   const grid = screen.getByTestId('grid');
   const squareCount = grid.childNodes.length;
 
-  expect(squareCount).toBe(64);
+  expect(squareCount).toBe(NUM_SQUARES);
 });
