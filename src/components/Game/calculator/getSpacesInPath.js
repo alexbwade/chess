db02102aef01028da038fae94f098fa624ac61f5
Alex Wade <alexbwade@gmail.com>
2022-05-15 13:26:25 -0700

    feat: add get spaces in path logic (#8)

--- src/components/Game/calculator/getSpacesInPath.js
+++ src/components/Game/calculator/getSpacesInPath.js
@@ -1,9 +1,39 @@
-export default function getSpacesInPath(move) {
-  // todo
+import { COLUMNS, ROWS } from "../../../constants";
+
+const getIncrementer = (diff) => {
+  if (diff > 0) return 1; // right, down
+  if (diff < 0) return -1; // left, up
+  return 0;
+};
+
+export function getSpacesInPathProperty(move) {
+  const { currentRowIndex, currentColIndex, targetRowIndex, targetColIndex, rowDiff, colDiff } = move;
+
   const results = [];
 
+  const rowIncrementer = getIncrementer(rowDiff);
+  const colIncrementer = getIncrementer(colDiff);
+
+  let thisRowIndex = currentRowIndex + rowIncrementer;
+  let thisColIndex = currentColIndex + colIncrementer;
+
+  while (!(thisRowIndex === targetRowIndex && thisColIndex === targetColIndex)) {
+    const rowId = ROWS[thisRowIndex];
+    const colId = COLUMNS[thisColIndex];
+    const squareId = `${rowId}${colId}`;
+
+    results.push(squareId);
+
+    thisRowIndex += rowIncrementer;
+    thisColIndex += colIncrementer;
+  }
+
+  return results;
+}
+
+export default function getSpacesInPath(move) {
   return {
     ...move,
-    spacesInPath: results,
+    spacesInPath: getSpacesInPathProperty(move),
   };
 }
